1cce3a2d28187b5c44208ded590d56824d19e5a4
nevesc <42561185+nevesc@users.noreply.github.com>
2018-09-20 09:00:17 -0300

    Update P0.c

--- P0.c
+++ P0.c
@@ -1,35 +1,20 @@
 /* P0.c */
-
 /* Gabriel Neves Cardoso */
-
-/* 11811ECP005 */
-
-
+/* 11811EBI014*/
 
 #include <stdio.h>
 
 int main(int argc, char ** argv) {
-
 int i;
+  if(argc > 1) {
+    if(argc > 2)
+    for(i = 0; i < argv[2][0]-'0'; i++)
+    printf("Valeu, %s\n", argv[1]);
 
-if(argc > 1) {
-
-if(argc > 2)
-
-for(i = 0; i < argv[2][0]-'0'; i++)
-
-printf("Valeu, %s\n", argv[1]);
-
-else
-
-printf("Saudacoes, %s\n", argv[1]);
-
-}
+  else
+    printf("Saudacoes, %s\n", argv[1]);}
 
 else
-
-printf("Ola!\n");
-
+  printf("Ola!\n");
 return 0;
-
-}
+  }
