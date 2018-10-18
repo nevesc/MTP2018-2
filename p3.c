8943f1d0f013690a396e6568533b13bddac9abb1
nevesc <42561185+nevesc@users.noreply.github.com>
2018-10-18 20:46:36 -0300

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,20 @@
+//Gabriel Neves
+//11811EBI014
+
+#include <stdio.h>
+int main()
+{
+	int conv=0, cum=0, i=0;
+	char numero[256];
+	printf("Digite os caracteres: "); 
+	scanf("%s", numero);
+	getchar();
+	for (i=0; numero[i]; i++)
+	{
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+			cum=cum*10+(numero[i]-'0');
+		else;
+	}
+	printf("%i", cum);
+	return 0;
+}
